aed8de8dab860c4b98fbbd257cd7506a7d5b7d99
The-King004 <139862820+The-King004@users.noreply.github.com>
2024-02-24 15:07:50 +0100

    version 1.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-const menuheader = document.querySelector(".Menu")
+const menuheader = document.querySelector(".menu")
 const nav_links = document.querySelector(".nav_links")
 
 menuheader.addEventListener("click", ()=>{
